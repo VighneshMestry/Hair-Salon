d880e581ae0c75d36cc8f03e09e5678690663b1c
vighnesh <mestryvighnesh27@gmail.com>
2023-11-06 20:33:41 +0530

    authentication progress

--- lib/main.dart
+++ lib/main.dart
@@ -22,3 +22,12 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+
+// // ...
+
+// await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+// );
